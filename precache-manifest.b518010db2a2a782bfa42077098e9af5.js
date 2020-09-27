4d8b0fcd9a377e681fa237f32b14d566e43e8ce9
Vinayak Nadar <vinu142@gmail.com>
2020-09-27 23:06:38 +1000

    Updates

---
+++ precache-manifest.b518010db2a2a782bfa42077098e9af5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d549689d4f8169c2dee7dddbbbd09890",
+    "url": "/react_pokedex/index.html"
+  },
+  {
+    "revision": "7e86a4e6a13c5a3cf668",
+    "url": "/react_pokedex/static/js/2.1c4512e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_pokedex/static/js/2.1c4512e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97402c61fe95f6c0e1f",
+    "url": "/react_pokedex/static/js/main.48d5d642.chunk.js"
+  },
+  {
+    "revision": "3eefcf062d4a4fb5f3ff",
+    "url": "/react_pokedex/static/js/runtime-main.6c9f8fe5.js"
+  }
+]);
